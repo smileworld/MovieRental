1798079b97ce390929f03877e3433b24df8ea4b7
smileworld <weikj8@gmail.com>
2012-03-10 23:15:19 +0800

    refactorying state4

--- Movie.java
+++ Movie.java
@@ -40,25 +40,7 @@ public class Movie {
   }
 
   double getCharge(int daysRented){
-	double result = 0;
-    switch(getPriceCode()){   //®æ
-        case Movie.REGULAR:                     //
-          result += 2;
-          if(daysRented>2)
-            result += (daysRented-2)*1.5;
-          break;
-
-        case Movie.NEW_RELEASE:         //
-          result += daysRented*3;
-          break;
-
-        case Movie.CHILDRENS:           //
-          result += 1.5;
-          if(daysRented>3)
-            result += (daysRented-3)*1.5;
-          break;
-      }
-	return result;
+	return _price.getCharge(daysRented);
   }
   
   int getFrequentRenterPoints(int daysRented){
@@ -110,21 +92,37 @@ public class Movie {
 }
 
 abstract class Price{
-  abstract int getPriceCode();
+  abstract int getPriceCode(); 
+  abstract double getCharge(int daysRented);
 }
 class ChildrensPrice extends Price{
   int getPriceCode(){
 	return Movie.CHILDRENS;
+  }
+  double getCharge(int daysRented){
+	double result = 1.5;
+	if(daysRented>3)
+       result += (daysRented-3)*1.5;
+	return result;
   }
 }
 class NewReleasePrice extends Price{
   int getPriceCode(){
 	return Movie.NEW_RELEASE;
   }
+  double getCharge(int daysRented){
+	return daysRented*3;
+  }
 }
 class RegularPrice extends Price{
   int getPriceCode(){
 	return Movie.REGULAR;
+  }
+  double getCharge(int daysRented){
+	double result = 2;
+	if(daysRented>2)
+       result += (daysRented-2)*1.5;
+	return result;
   }
 }
 class Rental {
