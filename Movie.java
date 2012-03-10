34ed6233ab2ef118b0ec9139788f578e108dacb6
smileworld <weikj8@gmail.com>
2012-03-10 22:26:11 +0800

    refactorying method3

--- Movie.java
+++ Movie.java
@@ -132,13 +132,11 @@ class Customer {
   }
 
   public String statement() {
-    int frequentRenterPoints = 0;       //
     Enumeration rentals = _rentals.elements();
     String result = "Rental Record for " + getName() + "\n";
 
     while(rentals.hasMoreElements()){
       Rental each = (Rental) rentals.nextElement(); //
-	  frequentRenterPoints += each.getFrequentRenterPoints();
 
       // show figures for this rental®Æ¡^
       result += "\t" + each.getMovie().getTitle() + "\t" +
@@ -147,11 +145,19 @@ class Customer {
 
     // add footer lines¡^
     result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
-    result += "You earned " + String.valueOf(frequentRenterPoints) +
+    result += "You earned " + String.valueOf(getTotalFrequentRenterPoints()) +
             " frequent renter points";
     return result;
   }
-  
+  private int getTotalFrequentRenterPoints(){
+	int result = 0;
+	Enumeration rentals = _rentals.elements();
+	while(rentals.hasMoreElements()){
+		Rental each = (Rental) rentals.nextElement();
+		result += each.getFrequentRenterPoints();
+	}
+	return result;
+  }
   private double getTotalCharge(){
 	double result = 0;
 	Enumeration rentals = _rentals.elements();
