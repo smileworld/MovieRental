f6c921ab913a9f38c0af5b06e5a9f9db41baecbb
smileworld <weikj8@gmail.com>
2012-03-11 20:50:26 +0800

    refactorying state 5

--- Movie.java
+++ Movie.java
@@ -94,6 +94,10 @@ public class Movie {
 abstract class Price{
   abstract int getPriceCode(); 
   abstract double getCharge(int daysRented);
+  
+  int getFrequentRenterPoints(int daysRented){
+      return 1;
+  }
 }
 class ChildrensPrice extends Price{
   int getPriceCode(){
@@ -112,6 +116,9 @@ class NewReleasePrice extends Price{
   }
   double getCharge(int daysRented){
 	return daysRented*3;
+  }
+  int getFrequentRenterPoints(int daysRented){
+      return (daysRented > 1) ? 2 : 1;
   }
 }
 class RegularPrice extends Price{
